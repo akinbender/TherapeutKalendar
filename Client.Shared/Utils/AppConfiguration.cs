50fdd2d86bea9857c18d241ac63255621ee207aa
akinbender <akinbender61@gmail.com>
2025-07-17 10:35:51 +0200

    config

--- Client.Shared/Utils/AppConfiguration.cs
+++ Client.Shared/Utils/AppConfiguration.cs
@@ -5,7 +5,7 @@ public class AppConfiguration
     public Theme Theme { get; set; } = Theme.Auto;
     public string[] SupportedCultures { get; } = ["en-US", "de-DE"];
     public string Language { get; set; } = "en-US";
-    public string BackendUrl { get; set; } = "http://localhost:8081";
+    public string BackendUrl { get; set; } = "http://91.99.223.23:8081";
     public bool AnalyticsEnabled { get; set; } = true;
     public DateTime? LastUpdated { get; set; }
 }
